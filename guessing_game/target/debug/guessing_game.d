d9cbb5fb93eade2068784bef0855b984dd6a7fd7
Johannes Katajamäki <jkatajamki@gmail.com>
2019-10-19 10:23:16 +0300

    Hello world

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/johannes/code/rust-stuff/guessing_game/target/debug/guessing_game: /home/johannes/code/rust-stuff/guessing_game/src/main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/johannes/code/rust-stuff/hello_cargo/target/debug/hello_cargo: /home/johannes/code/rust-stuff/hello_cargo/src/main.rs
