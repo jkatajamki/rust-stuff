67cc1fd60db5371912847dcbe6e71f5648a2c92b
Johannes Katajamäki <jkatajamki@gmail.com>
2020-07-08 23:07:41 +0300

    Add (failing) test

--- cacher/workout_app.rs
+++ cacher/workout_app.rs
@@ -51,3 +51,18 @@ fn main() {
 
   generate_workout(simulated_user_specified_value, simulated_random_number);
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let v1 = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
+  }
+}
