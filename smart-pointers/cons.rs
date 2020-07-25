c7359cd442fd6394e2c80e395409007506434bdc
Johannes Katajamäki <jkatajamki@gmail.com>
2020-07-25 13:51:05 +0300

    Rc exercise

--- smart-pointers/cons.rs
+++ smart-pointers/cons.rs
@@ -1,13 +1,15 @@
 enum List {
-  Cons(i32, Box<List>),
+  Cons(i32, Rc<List>),
   Nil,
 }
 
 use List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-  let list = Cons(1,
-    Box::new(Cons(2,
-      Box::new(Cons(3,
-        Box::new(Nil))))));
+  let a = Rc::new(Cons(5,
+    Rc::new(Cons(10,
+      Rc::new(Nil)))));
+  let b = Cons(3, Rc::clone(&a));
+  let c = Cons(4, Rc::clone(&a));
 }
