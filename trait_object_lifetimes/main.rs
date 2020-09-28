9b41e4e013eb2859f3d12bf523e3fdd1439aab38
Johannes Katajamäki <jkatajamki@gmail.com>
2020-09-28 22:40:42 +0300

    Trait object lifetimes

---
+++ trait_object_lifetimes/main.rs
@@ -0,0 +1,19 @@
+trait Red {
+
+}
+
+struct Ball<'a> {
+    diameter: &'a i32,
+}
+
+impl<'a> Red for Ball<'a> {
+
+}
+
+fn main() {
+    let num = 5;
+
+    let _obj = Box::new(Ball {
+        diameter: &num
+    }) as Box<dyn Red>;
+}
