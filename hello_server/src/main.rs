d4262cbb6e835f62a630380119cf8a0708cb28fe
Johannes Katajamäki <jkatajamki@gmail.com>
2020-10-11 09:49:58 +0300

    Single-threaded server

--- hello_server/src/main.rs
+++ hello_server/src/main.rs
@@ -1,16 +1,30 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND \r\n\r\n", "404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
 }
 
 fn main() {
