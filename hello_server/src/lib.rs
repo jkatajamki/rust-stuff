8fb0098ae2c0e651399bff583790eab4c75506ad
Johannes Katajamäki <jkatajamki@gmail.com>
2020-11-07 13:37:13 +0200

    Process requests asynchronously

--- hello_server/src/lib.rs
+++ hello_server/src/lib.rs
@@ -1,5 +1,12 @@
 use std::{error::Error, fmt};
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+mod fn_box;
+
+type Job = Box<dyn fn_box::FnBox + Send + 'static>;
 
 struct Worker {
     id: usize,
@@ -7,16 +14,27 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+
+                job.call_box();
+            }
+        });
+
         Worker {
             id,
-            thread: thread::spawn(|| {}),
+            thread,
         }
     }
 }
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 #[derive(Debug)]
@@ -34,12 +52,19 @@ impl ThreadPool {
     fn get_threads(size: usize) -> ThreadPool {
         let mut workers = Vec::with_capacity(size);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         for id in 0..size {
-            workers.push(Worker::new(id));
+            let cloned_receiver = Arc::clone(&receiver);
+
+            workers.push(Worker::new(id, cloned_receiver));
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender,
         }
     }
 
@@ -58,6 +83,8 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
 
+        self.sender.send(job).unwrap();
     }
 }
